f56aa80467dc976987ad2ee781148dd3e153703d
Koreoxy <rayssankn@gmail.com>
2024-03-07 15:39:49 +0700

    update : get user api

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { CreateUserRequest, LoginUserRequest } from "../model/user-model";
 import { UserService } from "../service/user-service";
+import { UserRequest } from "../type/user-request";
 
 export class UserController {
   // USER CONTROLLER REGISTER
@@ -21,6 +22,18 @@ export class UserController {
     try {
       const request: LoginUserRequest = req.body as LoginUserRequest;
       const response = await UserService.login(request);
+      res.status(200).json({
+        data: response,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  // GET
+  static async get(req: UserRequest, res: Response, next: NextFunction) {
+    try {
+      const response = await UserService.get(req.user!);
       res.status(200).json({
         data: response,
       });
